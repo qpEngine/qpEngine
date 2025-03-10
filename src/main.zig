b6bae70f8ad3ebbd6d4931c11bd6c32cbeaa8cac
qaptoR <rocco.ruscitti@outlook.com>
2025-03-10 06:11:39 -0700

    Regex class complete with full test coverage
    RegexMatch also complete, but lacking test coverage outside of
    use in Regex tests

--- src/main.zig
+++ src/main.zig
@@ -1,57 +1,48 @@
+//
+//
+//
+//
+//
+//    I. qpEngine
+//                                                         ,,
+//                      `7MM"""YMM                         db
+//                        MM    `7
+//      ,dW"Yvd`7MMpdMAo. MM   d    `7MMpMMMb.  .P"Ybmmm `7MM  `7MMpMMMb.  .gP"Ya
+//     ,W'   MM  MM   `Wb MMmmMM      MM    MM :MI  I8     MM    MM    MM ,M'   Yb
+//     8M    MM  MM    M8 MM   Y  ,   MM    MM  WmmmP"     MM    MM    MM 8M""""""
+//     YA.   MM  MM   ,AP MM     ,M   MM    MM 8M          MM    MM    MM YM.    ,
+//      'MbmdMM  MMbmmd'.JMMmmmmMMM .JMML  JMML.YMMMMMb  .JMML..JMML  JMML.`Mbmmd'
+//           MM  MM                            6'     dP
+//         .JMMLJMML.                          YbmmmdY'
+//
+//    II. Copyright (c) 2025-present Rocco Ruscitti
+//
+//    III. License
+//    This software is not yet licensed and is not available for use or distribution.
+//
+//
+//
+//
+//
+
+pub fn main() void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() != .leak);
+    const allocator = gpa.allocator();
+
+    var regex = Regex.init(allocator);
+    defer regex.deinit();
+
+    std.debug.print("Hello, qpEngine!\n", .{});
+}
+
 const std = @import("std");
 const re = @cImport({
     @cDefine("PCRE2_CODE_UNIT_WIDTH", "8");
     @cInclude("pcre2.h");
 });
 
+const Allocator = std.mem.Allocator;
+const Regex = @import("regex").Regex;
+
 const PCRE2_ZERO_TERMINATED = ~@as(re.PCRE2_SIZE, 0);
-
-pub fn main() void {
-    const pattern = "your\\s(.*)\\s";
-    const subject = "all of your codebase are belong to us!";
-    const rxp = compile(pattern);
-    const match = find(rxp.?, subject);
-    std.debug.print("Pattern is: {s}\n", .{pattern});
-    std.debug.print("Haystack is: {s}\n", .{subject});
-    std.debug.print("Match is: {?s}\n", .{match});
-}
-
-/// Compiles a regex pattern string and returns a pattern code you can use
-/// to match subjects. Returns `null` if something is wrong with the pattern
-fn compile(needle: []const u8) ?*re.pcre2_code_8 {
-    const pattern: re.PCRE2_SPTR8 = &needle[0];
-    var errornumber: c_int = undefined;
-    var erroroffset: re.PCRE2_SIZE = undefined;
-
-    const regex: ?*re.pcre2_code_8 = re.pcre2_compile_8(pattern, PCRE2_ZERO_TERMINATED, 0, &errornumber, &erroroffset, null);
-    return regex;
-}
-
-/// Takes in a compiled regexp pattern from `compile` and a string of test which is the haystack
-/// and returns the first match from the haystack.
-fn find(regexp: *re.pcre2_code_8, haystack: []const u8) ?[]const u8 {
-    const subject: re.PCRE2_SPTR8 = &haystack[0];
-    const subjLen: re.PCRE2_SIZE = haystack.len;
-
-    const matchData: ?*re.pcre2_match_data_8 = re.pcre2_match_data_create_from_pattern_8(regexp, null);
-    const rc: c_int = re.pcre2_match_8(regexp, subject, subjLen, 0, 0, matchData.?, null);
-
-    if (rc < 0) {
-        return null;
-    }
-
-    const ovector = re.pcre2_get_ovector_pointer_8(matchData);
-    if (rc == 0) {
-        std.debug.print("ovector was not big enough for all the captured substrings\n", .{});
-        return null;
-    }
-
-    if (ovector[0] > ovector[1]) {
-        std.debug.print("error with ovector\n", .{});
-        re.pcre2_match_data_free_8(matchData);
-        re.pcre2_code_free_8(regexp);
-        return null;
-    }
-    const match = haystack[ovector[0]..ovector[1]]; // First match only
-    return match;
-}
