48c8db830aee954a76156d2f0c09e375eaf065a4
qaptoR <git@arxdaingneachd.org>
2025-03-10 06:11:39 -0700

    Regex class complete with full test coverage
    RegexMatch also complete, but lacking test coverage outside of
    use in Regex tests

--- build.zig
+++ build.zig
@@ -4,22 +4,32 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const regex_mod = b.createModule(.{
+        .root_source_file = b.path("src/qp/regex/regex.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    exe_mod.addImport("regex", regex_mod);
+
     const exe = b.addExecutable(.{
         .name = "qpEngine",
         .root_module = exe_mod,
     });
 
-    const pcre2_dep = b.dependency("pcre2", .{
+    const pcre2_8_dep = b.dependency("pcre2", .{
         .target = target,
         .optimize = optimize,
     });
-    exe.linkLibrary(pcre2_dep.artifact("pcre2-8"));
+    regex_mod.linkLibrary(pcre2_8_dep.artifact("pcre2-8"));
+    exe.linkLibrary(pcre2_8_dep.artifact("pcre2-8"));
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -32,4 +42,13 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // TESTING
+    const regex_lib_unit_test = b.addTest(.{
+        .root_module = regex_mod,
+    });
+    const run_lib_unit_tests = b.addRunArtifact(regex_lib_unit_test);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
